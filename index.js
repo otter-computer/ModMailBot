7fca87a10a1205b20d8d20b2223e5b7e0f909af9
Thomas <thomas@otter.computer>
2021-09-21 00:09:55 +0200

    Thread-based modmail (#2)

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const Bot = require(`./Bot`);
-const express = require(`express`);
 
-const TemplateBot = new Bot();
-
-// Start webserver
-// Required for running on Azure WebApp
-const webserver = express();
-webserver.get(`/`, (req, res) => res.send(`ping`));
-webserver.listen(8080, () => {});
+const ModMailBot = new Bot();
 
 // Handle graceful shutdowns
 process.on(`SIGINT`, cleanup);
 process.on(`SIGTERM`, cleanup);
 
 function cleanup() {
-  TemplateBot.destroy();
+  ModMailBot.destroy();
   process.exit();
 }
 
-TemplateBot.connect();
+ModMailBot.connect();
