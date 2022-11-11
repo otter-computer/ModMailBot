317c95ef52b7f8065230c60e577dbcdc3f8a88c5
Thomas <thomas@otter.computer>
2022-11-11 22:01:12 +0000

    Remove Docker

--- index.js
+++ index.js
@@ -11,4 +11,10 @@ function cleanup() {
   process.exit();
 }
 
+// Basic HTTP server to keep Azure App Service happy
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('ModMailBot is alive!');
+}).listen(8080);
+
 ModMailBot.connect();
